4accb835b6e2d27a131b92175ad5e579eeace85a
Johnathan Bryce <johnathanbryce@gmail.com>
2026-06-10 15:24:42 -0700

    main scaffold

--- sandbox/interview-prep/deep-core/samples-app/backend/app/main.py
+++ sandbox/interview-prep/deep-core/samples-app/backend/app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
-
-load_dotenv()
+from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
+router = APIRouter()
+
+
+@router.get("/")
+def init():
+    return {"message": "hello world"}
+
+# must come after defined routes as it takes a snapshot the instant it's called
+app.include_router(router)
